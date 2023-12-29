2ef8a6a236ffaedc9598356d479ed52ca1aea5d6
Krishna-priya-uppalapati <98213560+Krishna-priya-uppalapati@users.noreply.github.com>
2023-12-29 09:56:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-import streamlit
-streamlit.write("<h1>this is my streamlit<h1>",unsafe_allow_html=True)
+import streamlit as st
+st.write("<h1>this is my streamlit<h1>",unsafe_allow_html=True)
+st.write("<h1 style='color:red'>this is my streamlit<h1>",unsafe_allow_html=True)
+st.file_uploader("upload a file")
+st.image('jpg','png','jpeg')
