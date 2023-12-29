0f98befd62a9f7321d3b2c8c8113552eeb936284
Krishna-priya-uppalapati <98213560+Krishna-priya-uppalapati@users.noreply.github.com>
2023-12-29 10:13:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
+#from PTL import Images
 st.write("<h1>this is my streamlit<h1>",unsafe_allow_html=True)
 st.write("<h1 style='color:red'>this is my streamlit<h1>",unsafe_allow_html=True)
 st.file_uploader("upload a file")
 st.image("https://rukminim1.flixcart.com/flap/64/64/image/69c6589653afdb9a.png?q=100")
+st.video("https://www.youtube.com/watch?v=YZieL0izb2Y")
+img=st.file_uploader("uploader img")
+st.image(img)
