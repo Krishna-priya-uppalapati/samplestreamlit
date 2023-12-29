03d6924935da8a5b8403d821b0275fb4a0c00309
Krishna-priya-uppalapati <98213560+Krishna-priya-uppalapati@users.noreply.github.com>
2023-12-29 10:04:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.write("<h1>this is my streamlit<h1>",unsafe_allow_html=True)
 st.write("<h1 style='color:red'>this is my streamlit<h1>",unsafe_allow_html=True)
 st.file_uploader("upload a file")
-st.image('jpg','png','jpeg')
+st.image("https://rukminim1.flixcart.com/flap/64/64/image/69c6589653afdb9a.png?q=100")
